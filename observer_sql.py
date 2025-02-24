a5fd7f587c2020800d9b3a706ba0f6e2022b4692
Theodore Niu <theodore.niu@gmail.com>
2025-02-24 02:20:52 +0000

    improved worker

---
+++ observer_sql.py
@@ -0,0 +1,49 @@
+import time
+import subprocess
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from logger import logger
+
+TARGET_SCRIPT = 'worker_sql.py'
+
+process = None
+
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        global process
+        if event.src_path.endswith(TARGET_SCRIPT):
+            logger.info(f"{TARGET_SCRIPT} updated, restarting...")
+
+            if process and process.poll() is None:
+                process.terminate()
+                try:
+                    process.wait(timeout=5)
+                except subprocess.TimeoutExpired:
+                    process.kill()
+
+            process = subprocess.Popen(['python', TARGET_SCRIPT])
+
+
+if __name__ == "__main__":
+
+    process = subprocess.Popen(['python', TARGET_SCRIPT])
+
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path='.', recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+        if process and process.poll() is None:
+            process.terminate()
+            try:
+                process.wait(timeout=5)
+            except subprocess.TimeoutExpired:
+                process.kill()
+    observer.join()
